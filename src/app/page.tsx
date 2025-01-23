cd886b6ac1ab7a50fe9398fd036ed882891caebd
Emily Berryman <emilyrberryman@gmail.com>
2025-01-23 15:46:42 +1300

    Add contact section & strikethrough link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
 import React from "react";
 import styled from "styled-components";
 import Footer from "@/components/Footer";
 import ImageWithText from "@/components/ImageWithText";
 import Spacer from "@/components/Spacer";
 import ProjectOverview from "@/components/ProjectOverview";
+import StrikethroughLink from "@/components/Link/StrikethroughLink";
 
 export default function Home() {
 	const projects = [
@@ -34,12 +34,41 @@ export default function Home() {
 			<Spacer size={160} />
 			<ImageWithText />
 			<WorkSection>
-				<SectionHeading>Work</SectionHeading>
+				<SectionHeading id="work">Work</SectionHeading>
 			</WorkSection>
 			<ProjectSection>
 				<ProjectOverview projects={projects} />
 			</ProjectSection>
-			<Spacer size={384} />
+			{/* <Spacer size={384} /> */}
+			<ContactSection>
+				<ContactHeading id="contact">Contact</ContactHeading>
+				<ContactStatus>
+					I&apos;m based in Wellington NZ and am available for remote work
+					opportunities.
+				</ContactStatus>
+				<LinkWrapper>
+					<StrikethroughLink
+						href="mailto:emilyrberryman@gmail.com"
+						linkText="Email"
+						color="var(--secondary-blue)"
+						target="_blank"
+					/>
+					-
+					<StrikethroughLink
+						href="linkedin"
+						linkText="LinkedIn"
+						color="var(--secondary-blue)"
+						target="_blank"
+					/>
+					-
+					<StrikethroughLink
+						href="github"
+						linkText="github"
+						color="var(--secondary-blue)"
+						target="_blank"
+					/>
+				</LinkWrapper>
+			</ContactSection>
 			<Footer />
 		</>
 	);
@@ -67,8 +96,7 @@ const WorkSection = styled.section`
 `;
 
 const ProjectSection = styled.section`
-	display: flex;
-	flex-direction: column;
+	display: block;
 `;
 
 const SectionHeading = styled.h2`
@@ -77,4 +105,30 @@ const SectionHeading = styled.h2`
 	text-transform: uppercase;
 	padding-top: 49px;
 	flex-basis: 33%;
+`;
+
+const ContactSection = styled.section`
+	background-color: var(--light-gray);
+	display: flex;
+	justify-content: flex-start;
+	flex-direction: column;
+	min-height: 180px;
+	padding: 48px 152px;
+	gap: 32px;
+	border-top: 1px solid black;
+`;
+
+const ContactHeading = styled.h3`
+	font-size: var(--font-size-h3);
+	text-transform: uppercase;
+`;
+
+const ContactStatus = styled.p`
+	font-size: var(--font-size-small);
+`;
+
+const LinkWrapper = styled.div`
+	display: flex;
+	gap: 16px;
+	font-size: var(--font-size-small);
 `;

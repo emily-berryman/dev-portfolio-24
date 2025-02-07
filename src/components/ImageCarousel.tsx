d0b74331ca80c061b6b206e2d9024c95e334aec3
Emily Berryman <emilyrberryman@gmail.com>
2025-02-07 16:17:36 +1300

    Add swiper carousel package

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,59 +1,112 @@
 "use client";
 
 import React from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import styled from "styled-components";
+import Image from "next/image";
+import { QUERIES } from "@/constants";
+import { A11y, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/a11y";
 
-function ImageCarousel() {
-	const settings = {
-		dots: true,
-		infinite: true,
-		speed: 800,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		autoplay: false,
-		autoplaySpeed: 4000,
+interface Image {
+	src: string;
+	alt: string;
+}
+
+interface ImageCarouselProps {
+	images: Array<Image>;
+	id: number;
+}
+
+function ImageCarousel({ images, id }: ImageCarouselProps) {
+	console.log("here are the images", images, id);
+
+	const sliderProps = {
+		spaceBetween: 22,
+		slidesPerView: 3,
+		loopAddBlankSlides: true,
+		loop: true,
+
+		breakpoints: {
+			1120: {
+				slidesPerView: 2,
+			},
+		},
 	};
+
+	// console.log("here are the images", images[0].src);
 	return (
-		<SliderContainer>
-			<Slider {...settings}>
-				<Slide>1</Slide>
-				<Slide>2</Slide>
-				<Slide>3</Slide>
-				<Slide>4</Slide>
-			</Slider>
-		</SliderContainer>
+		<>
+			<SwiperContainer>
+				<Swiper
+					onSlideChange={(swiper) => console.log("slide change", swiper)}
+					modules={[A11y, Autoplay]}
+					onSwiper={(swiper) => console.log(swiper)}
+					spaceBetween={22}
+					slidesPerView={1}
+					loopAddBlankSlides={true}
+					loop={true}
+					// autoplay={{
+					// 	delay: 5000,
+					// }}
+					breakpoints={{
+						720: {
+							slidesPerView: 3,
+						},
+						608: {
+							slidesPerView: 2,
+						},
+					}}
+				>
+					{images.map((image, index) => (
+						<SwiperSlide key={index}>
+							<StyledImage src={image.src} alt={image.alt} fill={true} />
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</SwiperContainer>
+		</>
 	);
 }
 
-const SliderContainer = styled.div`
-	.slick-list {
-		display: grid;
-		grid-template-columns: repeat(3, 1fr);
+const StyledImage = styled(Image)`
+	aspect-ratio: 5/3;
+	object-fit: cover;
+	min-width: 0; // to ensure that image will scale smaller
+	border-radius: 14px;
+`;
+const SwiperContainer = styled.div`
+	margin-inline: calc(-1 * var(--desktop-horizontal-margin) * 1.5);
+
+	@media ${QUERIES.mobileAndDown} {
+		margin-inline: 48px;
 	}
 
-	.slick-track {
-		display: flex;
-		gap: 22px;
-		margin-left: -164px;
+	@media ${QUERIES.smallMobileAndDown} {
+		margin-inline: 24px;
 	}
 
-	.slick-slide {
-		background-color: grey;
-		border: 1px solid black;
+	.swiper-slide {
 		height: 279px;
+		flex-shrink: 0;
 		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.slick-dots {
-		bottom: -30px;
 	}
 `;
 
-const Slide = styled.div``;
+const SlideShowWrapper = styled.div`
+	display: flex;
+	gap: 22px;
+	margin-inline: calc(-1 * var(--desktop-horizontal-margin) * 1.5);
+	overflow: hidden;
+	position: relative;
+	min-width: 100vw;
+`;
+const SlideShowImage = styled.div`
+	min-width: 462px;
+	height: 279px;
+	border: 1px solid black;
+	border-radius: 14px;
+`;
 
 export default ImageCarousel;

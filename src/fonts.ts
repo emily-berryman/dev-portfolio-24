4a7759cc83aef3443300b3052416c77b3cc997cf
Emily Berryman <emilyrberryman@gmail.com>
2025-02-27 11:34:38 +1300

    add fonts

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,13 +1,12 @@
 import { Tektur, IBM_Plex_Mono } from "next/font/google";
 
 export const ibmPlexMono = IBM_Plex_Mono({
-	weight: ["300", "400", "600", "700"], // load only needed weights
+	weight: ["300", "400", "600", "700"],
 	style: ["normal", "italic"],
 	subsets: ["latin"],
 	display: "swap",
 });
 
-// Tektur font with selected weight
 export const tektur = Tektur({
 	weight: ["600"],
 	style: ["normal"],

a3b17c764a1695bf0eda71532e89613135acc108
Emily Berryman <emilyrberryman@gmail.com>
2025-03-01 15:58:33 +1300

    Refactor projectOverview page

--- src/components/ProjectOverview.tsx
+++ src/components/ProjectOverview.tsx
@@ -1,28 +1,25 @@
 "use client";
-
 import React from "react";
 import { QUERIES } from "@/constants";
 import styled from "styled-components";
-import Spacer from "./Spacer";
-import ImageCarousel from "./ImageCarousel";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
 import Button from "./Button/Button";
 
 interface Image {
 	src: string;
 	alt: string;
+	title?: string;
 }
 
-interface Project {
+interface ProjectOverviews {
 	id: number;
 	title: string;
 	description: string;
+	url: string;
 	images: Array<Image>;
 }
 
 interface Props {
-	projects: Project[]; // projects prop is an array of Project objects, whose shape is defined above
+	projects: ProjectOverviews[];
 }
 
 function ProjectOverview({ projects }: Props) {
@@ -30,71 +27,116 @@ function ProjectOverview({ projects }: Props) {
 		<>
 			{projects.map((project) => (
 				<React.Fragment key={project.id}>
-					<ProjectWrapper key={project.id}>
-						<ProjectDetailsWrapper>
-							<ProjectHeading>{project.title}</ProjectHeading>
-							<ProjectDescription>{project.description}</ProjectDescription>
-							{/* Info on this here: https://nextjs.org/docs/pages/api-reference/components/link#if-the-child-is-a-custom-component-that-wraps-an-a-tag */}
-							<LinkWrapper>
-								<Button
-									variant={"primary"}
-									target={"_self"}
-									href={"case-study"}
-								>
-									Case Study
-								</Button>
-							</LinkWrapper>
-						</ProjectDetailsWrapper>
-						<ImageCarousel id={project.id} images={project.images} />
-					</ProjectWrapper>
-					<Spacer size={288} />
+					{/* Project Details */}
+					<ProjectDetailsWrapper>
+						<ProjectHeading>{project.title}</ProjectHeading>
+						<ProjectDescription>{project.description}</ProjectDescription>
+						<LinkWrapper>
+							<Button target={"_self"} href={project.url}>
+								View Project
+							</Button>
+						</LinkWrapper>
+					</ProjectDetailsWrapper>
+					{/* Project Images */}
+					<MultiImageWrapper>
+						{project.images.map((image, index) => (
+							<ImageWrapper key={index} index={index}>
+								<StyledImage src={image.src} alt={image.alt} />
+							</ImageWrapper>
+						))}
+					</MultiImageWrapper>
+					<Spacer />
 				</React.Fragment>
 			))}
 		</>
 	);
 }
 
+const MultiImageWrapper = styled.div`
+	display: flex;
+	margin-inline: var(--space-xl-2);
+	gap: var(--space-l);
+
+	@media ${QUERIES.smallMobileAndDown} {
+		margin-inline: var(--space-l);
+	}
+`;
+
+const ImageWrapper = styled.div<{ index: number }>`
+	display: flex;
+	width: 100%;
+	flex: 1;
+	border-radius: 8px;
+	overflow: hidden;
+	max-height: 500px;
+	box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+
+	${({ index }) =>
+		index === 1 &&
+		`@media (max-width: 900px) {
+			display: none;
+		 }`}
+
+	${({ index }) =>
+		index === 2 &&
+		`	@media ${QUERIES.laptopAndDown} {
+			display: none;
+		}`}
+`;
+
+const Spacer = styled.span`
+	display: inline-block;
+	height: 200px;
+
+	@media ${QUERIES.tabletAndDown} {
+		height: 148px;
+	}
+`;
+
+const StyledImage = styled.img`
+	object-fit: cover;
+	border-radius: 4px;
+	overflow: hidden;
+	aspect-ratio: 4/3;
+`;
+
 const ProjectDetailsWrapper = styled.div`
 	display: flex;
 	align-items: flex-start;
-	padding-bottom: 66px;
-	margin-inline: 152px;
-	gap: 24px;
+	margin-inline: var(--space-xl-2);
+	margin-bottom: var(--space-l);
+	gap: var(--space-l);
 
 	@media ${QUERIES.tabletAndDown} {
-		margin-inline: 80px;
 		flex-direction: column;
-	}
-
-	@media ${QUERIES.mobileAndDown} {
-		margin-inline: 48px;
+		margin-bottom: var(--space-xl-2);
 	}
 
 	@media ${QUERIES.smallMobileAndDown} {
-		margin-inline: 24px;
+		margin-inline: var(--space-l);
 	}
 `;
 
-const ProjectWrapper = styled.div`
-	padding-top: 48px;
-`;
-
 const ProjectHeading = styled.h3`
-	font-size: var(--font-size-h3);
+	font-size: var(--font-size-h2);
 	text-transform: uppercase;
-	/* padding-bottom: 16px; */
-	flex-basis: 33.33%;
+	flex-basis: 25%;
 `;
 
 const ProjectDescription = styled.span`
+	font-size: var(--font-size-h3);
 	display: inline-block;
-	flex-basis: 33.33%;
+	flex-basis: 50%;
+
+	@media ${QUERIES.tabletAndDown} {
+		margin-top: -8px; // to account for line height
+	}
 `;
 
 const LinkWrapper = styled.div`
 	display: flex;
-	justify-content: center;
-	flex-basis: 33.33%;
+	flex-basis: 25%;
+	justify-content: flex-end;
 `;
 
 export default ProjectOverview;

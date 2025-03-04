cb9d20f4c37e0011dcd9ab28754e8a41e91f771c
Emily Berryman <emilyrberryman@gmail.com>
2025-03-04 16:57:05 +1300

    Add css modules and refactor from styled-components

--- src/components/ImageWithText.tsx
+++ src/components/ImageWithText.tsx
@@ -1,80 +1,32 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
-import styled from "styled-components";
-import { QUERIES } from "@/constants";
+import styles from "./imagewithtext.module.css";
 
 function ImageWithText() {
 	return (
-		<SectionWrapper id="about">
-			<TextWrapper>
-				<IntroHeading>
+		<section className={styles.sectionWrapper} id="about">
+			<div className={styles.textWrapper}>
+				<h2 className={styles.introHeading}>
 					Creative at heart, developer by trade — <br /> I&apos;m passionate
 					about building web experiences that are impactful, purpose-driven and
 					accessible.
-				</IntroHeading>
-				<IntroParagraph>
+				</h2>
+				<p className={styles.introParagraph}>
 					With over a decade of experience in people-focused roles across the
 					tech, arts and hospitality industries, I love to collaborate and bring
 					curiosity, creativity and care to everything I create.
-				</IntroParagraph>
-			</TextWrapper>
-			<StyledImage src="/emily-profile.jpeg" alt="" width={279} height={279} />
-		</SectionWrapper>
+				</p>
+			</div>
+			<Image
+				className={styles.styledImage}
+				src="/emily-profile.jpeg"
+				alt=""
+				width={800}
+				height={800}
+				priority={true}
+			/>
+		</section>
 	);
 }
-
-const IntroHeading = styled.h2`
-	font-size: var(--font-size-highlight);
-	font-weight: var(--font-weight-light);
-	text-transform: uppercase;
-	padding-bottom: var(--space-m);
-
-	@media ${QUERIES.tabletAndDown} {
-		font-size: var(--font-size-h3);
-	}
-`;
-
-const IntroParagraph = styled.p`
-	font-size: var(--font-size-highlight);
-
-	@media ${QUERIES.tabletAndDown} {
-		font-size: var(--font-size-h3);
-		margin-bottom: var(--space-xl);
-	}
-`;
-
-const SectionWrapper = styled.section`
-	background-color: var(--light-grey);
-	display: flex;
-	flex-wrap: wrap;
-	flex-direction: column;
-	padding: 200px var(--space-xl-6);
-
-	@media ${QUERIES.tabletAndDown} {
-		padding: 150px var(--space-xl-2);
-	}
-
-	@media ${QUERIES.smallMobileAndDown} {
-		padding-inline: var(--space-xl);
-	}
-`;
-
-const StyledImage = styled(Image)`
-	aspect-ratio: 1/1;
-	object-fit: cover;
-	min-width: 0;
-`;
-
-const TextWrapper = styled.div`
-	flex: 1 1 auto;
-	max-width: 60%;
-	margin-left: auto;
-
-	@media (max-width: 57.5rem) {
-		max-width: fit-content;
-	}
-`;
 
 export default ImageWithText;
--- src/components/ProjectOverview.tsx
+++ src/components/ProjectOverview.tsx
@@ -1,13 +1,14 @@
-"use client";
 import React from "react";
-import { QUERIES } from "@/constants";
-import styled from "styled-components";
-import Button from "./Button/Button";
+import Button from "./Links/ButtonLink";
+import Image from "next/image";
+import styles from "./projectoverview.module.css";
 
 interface Image {
 	src: string;
 	alt: string;
 	title?: string;
+	width: number;
+	height: number;
 }
 
 interface ProjectOverviews {
@@ -28,115 +29,41 @@ function ProjectOverview({ projects }: Props) {
 			{projects.map((project) => (
 				<React.Fragment key={project.id}>
 					{/* Project Details */}
-					<ProjectDetailsWrapper>
-						<ProjectHeading>{project.title}</ProjectHeading>
-						<ProjectDescription>{project.description}</ProjectDescription>
-						<LinkWrapper>
-							<Button target={"_self"} href={project.url}>
+					<div className={styles.projectDetailsWrapper}>
+						<h3 className={styles.projectHeading}>{project.title}</h3>
+						<span className={styles.projectDescription}>
+							{project.description}
+						</span>
+						<div className={styles.linkWrapper}>
+							<Button target={"_self"} href={project.url} passHref>
 								View Project
 							</Button>
-						</LinkWrapper>
-					</ProjectDetailsWrapper>
+						</div>
+					</div>
 					{/* Project Images */}
-					<MultiImageWrapper>
+					<div className={styles.multiImageWrapper}>
 						{project.images.map((image, index) => (
-							<ImageWrapper key={index} index={index}>
-								<StyledImage src={image.src} alt={image.alt} />
-							</ImageWrapper>
+							<div
+								key={index}
+								className={`${styles.imageWrapper} ${
+									index === 1 && styles.hideOnMobile
+								} ${index === 2 && styles.hideOnLaptop}`}
+							>
+								<Image
+									className={styles.styledImage}
+									width={image.width}
+									height={image.height}
+									src={image.src}
+									alt={image.alt}
+								/>
+							</div>
 						))}
-					</MultiImageWrapper>
-					<Spacer />
+					</div>
+					<span className={styles.spacer} />
 				</React.Fragment>
 			))}
 		</>
 	);
 }
-
-const MultiImageWrapper = styled.div`
-	display: flex;
-	margin-inline: var(--space-xl-2);
-	gap: var(--space-l);
-
-	@media ${QUERIES.smallMobileAndDown} {
-		margin-inline: var(--space-l);
-	}
-`;
-
-const ImageWrapper = styled.div<{ index: number }>`
-	display: flex;
-	width: 100%;
-	flex: 1;
-	border-radius: 8px;
-	overflow: hidden;
-	max-height: 500px;
-	box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-
-	${({ index }) =>
-		index === 1 &&
-		`@media (max-width: 900px) {
-			display: none;
-		 }`}
-
-	${({ index }) =>
-		index === 2 &&
-		`	@media ${QUERIES.laptopAndDown} {
-			display: none;
-		}`}
-`;
-
-const Spacer = styled.span`
-	display: inline-block;
-	height: 200px;
-
-	@media ${QUERIES.tabletAndDown} {
-		height: 148px;
-	}
-`;
-
-const StyledImage = styled.img`
-	object-fit: cover;
-	border-radius: 4px;
-	overflow: hidden;
-	aspect-ratio: 4/3;
-`;
-
-const ProjectDetailsWrapper = styled.div`
-	display: flex;
-	align-items: flex-start;
-	margin-inline: var(--space-xl-2);
-	margin-bottom: var(--space-l);
-	gap: var(--space-l);
-
-	@media ${QUERIES.tabletAndDown} {
-		flex-direction: column;
-		margin-bottom: var(--space-xl-2);
-	}
-
-	@media ${QUERIES.smallMobileAndDown} {
-		margin-inline: var(--space-l);
-	}
-`;
-
-const ProjectHeading = styled.h3`
-	font-size: var(--font-size-h2);
-	text-transform: uppercase;
-	flex-basis: 25%;
-`;
-
-const ProjectDescription = styled.span`
-	font-size: var(--font-size-h3);
-	display: inline-block;
-	flex-basis: 50%;
-
-	@media ${QUERIES.tabletAndDown} {
-		margin-top: -8px; // to account for line height
-	}
-`;
-
-const LinkWrapper = styled.div`
-	display: flex;
-	flex-basis: 25%;
-	justify-content: flex-end;
-`;
 
 export default ProjectOverview;
